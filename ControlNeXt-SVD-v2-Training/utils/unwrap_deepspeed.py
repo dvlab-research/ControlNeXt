d463473239e5293e20942f4f183214337fd12544
Pbihao <1435343052@qq.com>
2024-09-25 14:52:42 +0800

    Update unwrap_deepspeed.py

--- ControlNeXt-SVD-v2-Training/utils/unwrap_deepspeed.py
+++ ControlNeXt-SVD-v2-Training/utils/unwrap_deepspeed.py
@@ -16,19 +16,19 @@ if __name__ == "__main__":
     state_dict = torch.load(os.path.join(args.checkpoint_dir, "pytorch_model.bin"))
 
     unet_state = OrderedDict()
-    controlnet_state = OrderedDict()
+    controlnext_state = OrderedDict()
     for name, data in state_dict.items():
         model = name.split('.', 1)[0]
         module = name.split('.', 1)[1]
         if model == 'unet':
             unet_state[module] = data
-        elif model == 'controlnet':
-            controlnet_state[module] = data
+        elif model == 'controlnext':
+            controlnext_state[module] = data
     
-    for model in ['unet', 'controlnet']:
+    for model in ['unet', 'controlnext']:
         if not os.path.exists(os.path.join(args.checkpoint_dir, model)):
             os.makedirs(os.path.join(args.checkpoint_dir, model))
 
 
     torch.save(unet_state, os.path.join(args.checkpoint_dir, "unet", "diffusion_pytorch_model.bin"))
-    torch.save(controlnet_state, os.path.join(args.checkpoint_dir, "controlnet", "diffusion_pytorch_model.bin"))
+    torch.save(controlnext_state, os.path.join(args.checkpoint_dir, "controlnext", "diffusion_pytorch_model.bin"))
